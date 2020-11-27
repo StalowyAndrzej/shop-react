af61075987b69f28956757ad5df4a5fefcc69732
Andrzej Karawaj <andrzejkar17@gmail.com>
2020-11-27 18:39:31 +0100

    Create menu item component

---
+++ src/components/menu-item/menu-item.tsx
@@ -0,0 +1,22 @@
+import React, {FunctionComponent} from 'react';
+
+interface MenuProps {
+    title: string,
+}
+
+
+
+const MenuItem: FunctionComponent<MenuProps> = ({title})=> (
+    
+        <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>{title}</h1>
+                    <span className='subtitle'>Buy now</span>
+                </div>
+
+            </div>
+    
+)
+
+
+export default MenuItem;
