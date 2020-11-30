372f33c63661164b5b93a8a13049a9e750882bb0
Andrzej Karawaj <andrzejkar17@gmail.com>
2020-11-30 15:02:00 +0100

    Move app component to folder

--- src/components/menu-item/menu-item.tsx
+++ src/components/menu-item/menu-item.tsx
@@ -9,10 +9,11 @@ interface IProps {
 }
 
 const MenuItem: FC<IProps> = ({ title, imageUrl, size }) => (
-  <div
-    style={{ backgroundImage: `url(${imageUrl})` }}
-    className={`${size} menu-item`}
-  >
+  <div className={`${size} menu-item`}>
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className="background-image"
+    />
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">Buy now</span>
