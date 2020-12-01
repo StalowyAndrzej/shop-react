355ba64650e2bc8ef5ad1e578f6f8fd8ecaea6cc
Andrzej Karawaj <andrzejkar17@gmail.com>
2020-12-01 12:29:25 +0100

    Add react-router browser

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,24 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
 import HomePage from '../../pages/homepage/homepage'
+import {Switch, Route} from 'react-router-dom'
 
 
+const HatsPage:React.FC = () => (
+  <div>
+    <h1>hello HATS</h1>
+  </div>
+)
 
 class App extends React.Component<Record<string, unknown>, undefined> {
   public render() {
     return (
-     <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route  path='/hats' component={HatsPage} />
+     </Switch>
+   </div>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 import { render } from "react-dom";
 import App from "./components/app/App";
+import { BrowserRouter } from "react-router-dom";
 
 const rootEl = document.getElementById("root");
 
-render(<App />, rootEl);
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootEl,
+);
