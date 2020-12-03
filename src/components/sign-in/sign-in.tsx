90d702ee44242285413f95b91e136fb8e0d0811d
Andrzej Karawaj <andrzejkar17@gmail.com>
2020-12-03 17:10:20 +0100

    Create sign in component

---
+++ src/components/sign-in/sign-in.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import './sign-in.styles.scss'
+import SignInAndSignUpPage from '../../pages/sign-in-and-sign-up/sign-in-and-sign-up';
+
+
+interface IProps {}
+
+interface ISignInState {
+    email: string,
+    password: string,
+}
+
+type NameAndValue ={
+    email: string;
+    password: string;
+}
+
+class SignIn extends React.Component<IProps, ISignInState> {
+    constructor(props: IProps){
+        super(props)
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+
+        this.setState({email: '', password: ''})
+    }
+
+
+    // handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const {value, name} = e.target;
+    //     this.setState({[name]: value})
+    // }
+
+    render(){
+        return(
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input name='email'  type='email' value={this.state.email} required/><label>Email</label>
+                    <input name='password'  type='password' value={this.state.password} required/><label>Password</label>
+                    <input type='submit' value='Submit form'/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
