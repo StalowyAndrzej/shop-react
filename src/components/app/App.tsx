17ce99cfbf6212c549807b16fd29463cd4c98b1b
Andrzej Karawaj <andrzejkar17@gmail.com>
2020-12-03 13:43:46 +0100

    Create header component and global styles

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
 import { Switch, Route } from "react-router-dom";
+import "../../assets/scss/App.scss"
 import HomePage from "../../pages/homepage/homepage";
 import ShopPage from "../../pages/shop/shoppage";
 
 class App extends React.Component<Record<string, unknown>, undefined> {
   public render() {
     return (
-      <div>
+      <div className="app">
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
